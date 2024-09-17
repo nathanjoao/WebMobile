b08ab048eab5962d3b8293e7e7a9b8b3f0beb7de
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2024-09-16 21:58:07 -0300

    .

---
+++ Node/app.js
@@ -0,0 +1,8 @@
+const matematica = require('./matematica');
+
+const resultado = matematica.sub(5, 2);
+console.log(`Resultado da subtração : ${resultado}`);
+
+const resultado2 = matematica.somar(1, 1);
+console.log(`Resultado da adição : ${resultado}`);
+
---
+++ Node/matematica.js
@@ -0,0 +1,12 @@
+function somar(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+module.exports = {
+    somar, 
+    sub
+};
