56df57fea761e0b674feab1986df7a25770b9bce
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2024-09-18 13:49:13 -0300

    .

--- Node/app.js
+++ Node/app.js
@@ -3,6 +3,6 @@ const matematica = require('./matematica');
 const resultado = matematica.sub(5, 2);
 console.log(`Resultado da subtração : ${resultado}`);
 
-const resultado2 = matematica.somar(1, 1);
-console.log(`Resultado da adição : ${resultado}`);
+const resultado2 = matematica.somar(1, 5);
+console.log(`Resultado da adição : ${resultado2}`);
 
