72697e1a238355f9593932eb63fa7e4ad1ec8948
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2024-09-23 18:36:55 -0300

    .

---
+++ JavaScript/Aula4-Modules/JS/App.js
@@ -0,0 +1,3 @@
+import { adicionarLivro, listarLivros } from './JS/Biblioteca';
+
+adicionarLivro("Dom Casmurro", "Machado de Assis", 1899, "Romance");
---
+++ JavaScript/Aula4-Modules/JS/Biblioteca.js
@@ -0,0 +1,13 @@
+let bilioteca = [{
+    titulo: "",
+    autor: "",
+    anoPub: "",
+    genero: "",
+    disponibilidade: true
+}];
+
+function listarLivros() {
+    return biblioteca;
+}
+
+export { adicionarLivro, listarLivros };
